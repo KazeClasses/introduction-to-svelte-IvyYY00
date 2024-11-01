806a7a4e745ddeaf77a13e999805efd00f9ea98a
IvyYY00 <ivyyouy@gmail.com>
2024-10-31 20:24:03 -0400

    10.31

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  
+  let name = 'Ivy';
+  let count = 0;
+  
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+
+{#if name === 'Ivy'}
+  <h1>Hello {name}!</h1>
+{:else}
+  <h1>I don't say hello to anyone else</h1>
+{/if}
+
+<button on:click={handleClick}>
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+<input bind:value={name} placeholder="Enter your name" />
+<p>Happy Halloween {name}!</p>
